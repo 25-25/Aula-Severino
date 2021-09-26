dbcfbc6ed3c86e3ac1646f1dd3c3252b3ddc3b3e
25-25 <38917618+25-25@users.noreply.github.com>
2021-09-26 19:58:34 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+void main() {
+ runApp(
+ Center(
+ child: Text(
+ 'Thainá Caroline',
+ textDirection: TextDirection.ltr,
+ ), //Text.
+ ), //Center.
+ );
+}
+

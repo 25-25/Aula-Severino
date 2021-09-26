e7d7b46a79e37df6ff798cf9b9d174f43b0a5e5b
25-25 <38917618+25-25@users.noreply.github.com>
2021-09-26 20:14:45 -0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
 void main() {
+ String nome = "Thaina";
  runApp(
  Center(
- child: Text(
- 'Thainá Caroline',
+ child: RichText(
  textDirection: TextDirection.ltr,
- ), //Text.
- ), //Center.
+ text: TextSpan(
+ text: "Olá, tudo bem? ",
+ style: TextStyle(
+ color: Colors.green,
+ fontSize: 30,
+ fontWeight: FontWeight.bold,
+ backgroundColor: Colors.black,
+ ), //TextStyle.
+ children: <TextSpan>[
+ TextSpan(
+ text: '$nome',
+ style: TextStyle(
+ color: Colors.blue,
+ decoration: TextDecoration.underline,
+ decorationColor: Colors.red,
+ decorationStyle: TextDecorationStyle.double,
+ ),
+ ),
+ TextSpan(
+ text: ' seja bem vindo ao meu programa ',
+ ),
+ ],
+ ),
+ ),
+ ),
  );
 }
-
